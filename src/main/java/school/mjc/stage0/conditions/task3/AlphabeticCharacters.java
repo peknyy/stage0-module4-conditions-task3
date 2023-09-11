9e0ab0323ef3e35fbdedb9c90711cea58c389683
peknyy <lukcymr3@gmail.com>
2023-09-11 13:58:39 +0600

    Done

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if (character >= 65 && character <= 90 || character >= 97 && character <= 122){
+            character = Character.toUpperCase(character);
+            if(character == 65 || character == 69 || character == 73 || character == 79 || character == 85) System.out.println("Vowel");
+            else System.out.println("Consonant");
+        }
+        else System.out.println("wrong alphabet!");
     }
 }
